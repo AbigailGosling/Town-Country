245413b212850b6599af611e1467b56f0f0a4d5d
Kez <kez@devclever.co.uk>
2020-07-10 14:39:07 +0100

    fix: fixPurchaseDB multi query

--- fixPurchaseDB.php
+++ fixPurchaseDB.php
@@ -7,7 +7,7 @@
             UPDATE `purchase_form` SET units = replace(units,',','|');
             UPDATE `purchase_form` SET species = replace(species,',','|');
             UPDATE `purchase_form` SET cut = replace(cut,',','|');";
-    $y = mysqli_query($conn,$x);
+    $y = mysqli_multi_query($conn,$x);
 
     echo mysqli_affected_rows($conn) . ' rows';
 ?>
