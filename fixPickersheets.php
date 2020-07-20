477579bfd593ac5008fad1253599f56bd7d8c99a
Kez <kez@devclever.co.uk>
2020-07-20 14:25:29 +0100

    fix: pickersheet items marked as sold

---
+++ fixPickersheets.php
@@ -0,0 +1,21 @@
+<?php
+    include_once('functions.php');
+
+
+    # grab all pickersheets that are marked as completed
+
+    $yPickersheets = mysqli_query($conn, "SELECT * FROM `pickersheets` WHERE completed =1");
+    $count = mysqli_num_rows($yPickersheets);
+
+    echo 'There are '. $count .' completed picksheets<br/><br/>';
+
+
+    while($pickersheet = mysqli_fetch_array($yPickersheets)){
+        $pickersheet_id = $pickersheet['id'];
+        
+        # foreach completed pickersheet, mark all the pickeritems as picked
+        $y = mysqli_query($conn, "UPDATE `pickeritems` SET status=1 WHERE pickersheet_id='$pickersheet_id'");
+        $affected = mysqli_affected_rows($conn);
+        echo 'Picksheet #' . $pickersheet_id . ' => '. $affected . ' rows updated<br/><Br/>'; 
+    }
+?>
