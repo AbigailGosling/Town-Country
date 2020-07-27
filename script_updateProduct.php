329cee725fb8ea8a9f7b94202f51d1c68fa041f0
Kez <kez@devclever.co.uk>
2020-07-27 16:18:51 +0100

    fix: fixed weights resetting to available after being edited

--- script_updateProduct.php
+++ script_updateProduct.php
@@ -57,7 +57,7 @@
 		
 		$weightVal = mysqli_real_escape_string($conn, $_POST['weight'.$weightid]);
 		
-		$xxx = "UPDATE `weights` SET status_id='0', product_id='$product_id',weight_gross='$weightVal',weight_tear='$weightVal' WHERE id='$weightid'";
+		$xxx = "UPDATE `weights` SET product_id='$product_id',weight_gross='$weightVal',weight_tear='$weightVal' WHERE id='$weightid'";
 		
 		$y = mysqli_query($conn, $xxx);
 	}
