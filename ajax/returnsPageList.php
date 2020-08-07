ab203d6b0bca44cdca7d55cdc14b5f013619404f
Kez <kez@devclever.co.uk>
2020-08-07 14:34:49 +0100

    fix: renaming & tidying returns code

--- ajax/returnsPageList.php
+++ ajax/returnsPageList.php
@@ -4,91 +4,73 @@
 	
 	$term = $_POST['searchterm'];
 	
-	if($term != ''){ ?>
-		<?php
+	if($term != ''){
 		
-		$x = "SELECT * FROM `customers` WHERE `businessname` LIKE '$term%'";
-		$y = mysqli_query($conn, $x);
-		
-		$supplierids = '';
-		
-		while($row = mysqli_fetch_array($y)){
-			$rowid = $row['id'];
-			$supplierids .= " OR supplier_id='$rowid'";
-		}
+		# Get any customers that match the search term
+		$customerQuery = mysqli_query($conn, "SELECT * FROM `customers` WHERE `businessname` LIKE '$term%'");
+		$customerIDs = array(0);
+		while($customer = mysqli_fetch_array($customerQuery)){ array_push($customerIDs, $customer['id']); }
+		$customerIDs = implode(',', $customerIDs);
 		
 		
-		
-		if (validateDate($term)) {
-			// echo $term;
-			// echo '<br/><br/>';
+		if(validateDate($term)) { # search term is a DATE
 			$date = str_replace('/', '-', $term);
 			$termDate = date('Y-m-d', strtotime($date));
-			// echo '<br/><br/>';
-			// echo $termDate = date('Y-m-d',$term);
-			// echo		$dateThing = new DateTime($term);
-			// $termDate = $dateThing->format('Y-m-d');
-			// echo 'isdate';
-			
-			$x = "SELECT * FROM `intake` WHERE returned=1 && date_received LIKE '%$termDate%' ORDER BY date_received DESC";
-			 
+			$searchQuery = "SELECT * FROM `intake` WHERE returned=1 && date_received LIKE '%$termDate%' ORDER BY date_received DESC";
 		}else{
-			// echo 'isnot_date';
-			$x = "SELECT * FROM `intake` WHERE returned=1 && (id='" . $term . "' OR returned=1 && vehicle_reg LIKE '%$term%' OR returned=1 && id LIKE '%$term%' OR returned=1 && delivery_note_number LIKE '%$term%' $supplierids) ORDER BY date_received DESC";
+			$searchQuery = "SELECT * FROM `intake` WHERE returned=1 && (id='" . $term . "' OR returned=1 && vehicle_reg LIKE '%$term%' OR returned=1 && id LIKE '%$term%' OR returned=1 && delivery_note_number LIKE '%$term%' OR returned=1 && supplier_id IN ($customerIDs)) ORDER BY date_received DESC";
 		}
 		
-		$y = mysqli_query($conn, $x) or die(mysqli_error($conn));
-			$count = mysqli_num_rows($y);
+		$searchResults = mysqli_query($conn, $searchQuery) or die(mysqli_error($conn));
+		$countResults = mysqli_num_rows($searchResults);
 	
-	if($count == 0){
-		?><h2 style="color:#fff;font-size:12px;">No intakes found</h2><?php
-	}else{
-		
-		while($row = mysqli_fetch_array($y)){
-		    $date_received = date('d/m/Y', strtotime($row['date_received']));
-		?>
-			<tr><td align="center" class="pos">
-				<a href="intake.php?id=<?php echo $row['id']; ?>" class="intake">
-					<table width="100%" border="0">
-						<tr>
-							<?php
-								$customer = getCustomer($row['supplier_id']);
-							?>
-							<td width="100" align="left">ID: I-0000<?php echo $row['id']; ?></td>
-							<td align="center" style="font-size: 18px;"><?php echo $customer['businessname']; ?></td>
-							<td width="100" align="right"><?php echo $date_received; ?></td>
-						</tr>
-					</table>
-				</a>
-				
-				<a href="javascript:;" onclick="deleteRow('<?php echo $row['id'];?>')" id="delete_intake"><i class="fa fa-times" aria-hidden="true"></i></a>
-			</td></tr>
-		<?php
+		if($countResults == 0){
+			?><h2 style="color:#fff;font-size:12px;">No intakes found</h2><?php
+		}else{
+			while($returnedIntake = mysqli_fetch_array($searchResults)){
+				$date_received = date('d/m/Y', strtotime($returnedIntake['date_received']));
+			?>
+				<tr><td align="center" class="pos">
+					<a href="intake.php?id=<?php echo $returnedIntake['id']; ?>" class="intake">
+						<table width="100%" border="0">
+							<tr>
+								<?php
+									$customer = getCustomer($returnedIntake['supplier_id']);
+								?>
+								<td width="100" align="left">ID: I-0000<?php echo $returnedIntake['id']; ?></td>
+								<td align="center" style="font-size: 18px;"><?php echo $customer['businessname']; ?></td>
+								<td width="100" align="right"><?php echo $date_received; ?></td>
+							</tr>
+						</table>
+					</a>
+					
+					<a href="javascript:;" onclick="deleteRow('<?php echo $returnedIntake['id'];?>')" id="delete_intake"><i class="fa fa-times" aria-hidden="true"></i></a>
+				</td></tr>
+			<?php
+			}
 		}
-	}
-	 }else{ ?>
-		<?php
+	}else{ # Search term is empty, show all returned intakes
 		
-		$x = "SELECT * FROM `intake` WHERE returned='1' ORDER BY date_received DESC";;
-		$y = mysqli_query($conn, $x) or die(mysqli_error($conn));
-		while($row = mysqli_fetch_array($y)){
-		    $date_received = date('d/m/Y', strtotime($row['date_received']));
+		$searchResults = mysqli_query($conn, "SELECT * FROM `intake` WHERE returned='1' ORDER BY date_received DESC") or die(mysqli_error($conn));
+
+		while($returnedIntake = mysqli_fetch_array($searchResults)){
+		    $date_received = date('d/m/Y', strtotime($returnedIntake['date_received']));
 		?>
 			<tr><td align="center" class="pos">
-				<a href="intake.php?id=<?php echo $row['id']; ?>" class="intake">
+				<a href="intake.php?id=<?php echo $returnedIntake['id']; ?>" class="intake">
 					<table width="100%" border="0">
 						<tr>
 							<?php
-								$customer = getCustomer($row['supplier_id']);
+								$customer = getCustomer($returnedIntake['supplier_id']);
 							?>
-							<td width="100" align="left">ID: I-0000<?php echo $row['id']; ?></td>
+							<td width="100" align="left">ID: I-0000<?php echo $returnedIntake['id']; ?></td>
 							<td align="center" style="font-size: 18px;"><?php echo $customer['businessname']; ?></td>
 							<td width="100" align="right"><?php echo $date_received; ?></td>
 						</tr>
 					</table>
 				</a>
 				
-				<a href="javascript:;" onclick="deleteRow('<?php echo $row['id'];?>')" id="delete_intake"><i class="fa fa-times" aria-hidden="true"></i></a>
+				<a href="javascript:;" onclick="deleteRow('<?php echo $returnedIntake['id'];?>')" id="delete_intake"><i class="fa fa-times" aria-hidden="true"></i></a>
 			</td></tr>
 		<?php
 		}
--- returnsList.php
+++ returnsList.php
@@ -13,12 +13,14 @@
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+	<script type="text/javascript" src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>		
 	<script>
 	$( function() {
 		$( "#datepicker" ).datepicker();
 	});
 	</script>
 </head>
+
 <body class="menu">
 <div id="top">
 	<a href="menu.php" id="menu">MENU</a>
@@ -29,7 +31,7 @@
 		<h1 class="int">Returns Intake LIST</h1>
 		<input type="text" id="instantSearch" placeholder="Search.." style="width:260px;height:28px;padding-left:10px;">
 		
-		<a href="intakeList.php" class="resetBtn">Clear</a>
+		<a href="returnsList.php" class="resetBtn">Clear</a>
 		<div class="datesearchcontainer">
 			<label>MONTH</label>
 			<select id="month">
@@ -52,32 +54,24 @@
 		</div>
 		<table width="100%" border="0" cellpadding="0" cellspacing="0" id="intakeAjax">
 			<?php
-				$x = "SELECT * FROM `intake` WHERE returned=1 ORDER BY date_received DESC";
-				$y = mysqli_query($conn, $x) or die(mysqli_error($conn));
-				while($row = mysqli_fetch_array($y)){
-				
-				
-				
-				$date_received = date('d/m/Y', strtotime($row['date_received']));
+				$queryResults = mysqli_query($conn, "SELECT * FROM `intake` WHERE returned=1 ORDER BY date_received DESC");
+				while($returnedIntake = mysqli_fetch_array($queryResults)){
+					$date_received = date('d/m/Y', strtotime($returnedIntake['date_received']));
 				?>
 				<tr><td align="center" class="pos">
-
-					<a href="intake.php?id=<?php echo $row['id']; ?>" class="intake">
+					<a href="intake.php?id=<?php echo $returnedIntake['id']; ?>" class="intake">
 						<table width="100%" border="0">
 							<tr>
 								<?php
-									$customer = getCustomer($row['supplier_id']);
+									$customer = getCustomer($returnedIntake['supplier_id']);
 								?>
-								<td width="100" align="left">ID: I-0000<?php echo $row['id']; ?></td>
+								<td width="100" align="left">ID: I-0000<?php echo $returnedIntake['id']; ?></td>
 								<td align="center" style="font-size: 18px;"><?php echo $customer['businessname']; ?></td>
 								<td width="100" align="right"><?php echo $date_received; ?></td>
 							</tr>
 						</table>
 					</a>
-					
-					 
-					<a href="javascript:;" onclick="deleteRow('<?php echo $row['id'];?>')" id="delete_intake"><i class="fa fa-times" aria-hidden="true"></i></a>
-
+					<a href="javascript:;" onclick="deleteRow('<?php echo $returnedIntake['id'];?>')" id="delete_intake"><i class="fa fa-times" aria-hidden="true"></i></a>
 				</td></tr>
 				<?php
 				}
@@ -149,10 +143,18 @@
 		}
 		
 		function deleteRow(intake_id, pallet_id){
-			if(confirm('Are you sure you want to delete this?')){
-				window.location.href = "/scripts/deleteIntake.php?intake_id=" + intake_id;
-				// console.log(intake_id + '  ' + pallet_id);
-			}
+			swal({
+				title: "Are you sure?",
+				text: "Intake #"+ intake_id + " will be deleted",
+				icon: "warning",
+				buttons: true,
+				dangerMode: true,
+			})
+			.then((confirmed) => {
+				if (confirmed) {
+					window.location.href = "/scripts/deleteIntake.php?intake_id=" + intake_id;
+				}
+			});
 		}
 	</script>
 </body>
