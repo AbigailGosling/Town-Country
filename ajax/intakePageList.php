00b66bb9f3ed26e6a17005595f16020877b12be1
Kez <kez@devclever.co.uk>
2021-02-16 11:01:25 +0000

    fix: alignment of ticks on intake list pages

--- ajax/intakePageList.php
+++ ajax/intakePageList.php
@@ -47,9 +47,13 @@
                 <tr><td align="center" class="pos">
                     <a href="intake.php?id=<?php echo $intake['id']; ?>" class="intake">
                         <table width="100%" border="0">
-                            <tr>
-                                <td width="30%" align="left">ID: I-0000<?php echo $intake['id']; ?></td>
-                                <td align="left" style="font-size: 18px;">
+						<tr>
+							<?php
+								$r = intakePriceComplete($intake['id']);    
+							?>
+							<td width="30%" align="left">
+								ID: I-0000<?php echo $intake['id'];?></td>
+							<td align="left" style="font-size: 18px;" class="<?php if($r == 1){ echo 'flex space-between v-center'; } ?>">
 								<?php
 
 									if($intake['returned'] == '1'){
@@ -63,16 +67,15 @@
 									}else{
 										echo supplierName($intake['supplier_id']);
 									}
+									if($intake['returned'] == '1'){ echo ' <small class="return-highlight">return entry</small>'; }
 
-									$r = intakePriceComplete($intake['id']);    
 									if($r == 1){
 									?><i class="fa fa-check" aria-hidden="true" style="margin-left:10px;"></i><?php
 									}
-									if($intake['returned'] == '1'){ echo ' <small class="return-highlight">return entry</small>'; }
-									?>
-                                </td>
-                                <td width="30%" align="right"><?php echo $date_received; ?></td>
-                            </tr>
+								?>
+							</td>
+							<td width="30%" align="right"><?php echo $date_received; ?></td>
+						</tr>
                         </table>
                     </a>
                     
