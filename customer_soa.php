ae65a60e69c93246d919df038b53d68ca10d7897
kezhall1307 <kez@devclever.co.uk>
2021-03-29 12:27:06 +0100

    fix: (TC-257) column tweaks to SOA table

--- customer_soa.php
+++ customer_soa.php
@@ -40,14 +40,13 @@
             }
         ?>
     </h2>
-    <a class="mp" href="/multi_invoice_payments.php?customer_id=<?php echo $_GET['id']; ?>">Make Payment</a>
+    <a class="mp" href="/multi_invoice_payments.php?customer_id=<?php echo $_GET['id']; ?>">Make / View payments</a>
     <table class="table" width="100%">
         <tr class="heading">
             <th align="left">Invoice ID</th>
             <th align="left">Add Payment</th>
             <th align="left">Due Date</th>
             <th align="left">Date</th>
-            <th align="left">PDF</th>
             <th align="right">Price</th>
             <th align="right">Paid</th>
             <th align="right">Outstanding</th>
@@ -99,7 +98,7 @@
 			<tr class="<?php  if($i%2 == 0){ echo 'odd'; }else{ echo 'even'; } ?>">
 				<td><a href="/invoice.php?id=<?php echo $picksheet['id']; ?>"><?php echo $picksheet['id']; ?></a></td>
                 <?php if(!$invoicePaid) { ?>
-                    <td><a href="/single_invoice_payments.php?customer_id=<?php echo $_GET['id']; ?>&invoice_id=<?php echo $picksheet['id']; ?>">Make Payment</a></td>
+                    <td><a href="/single_invoice_payments.php?customer_id=<?php echo $_GET['id']; ?>&invoice_id=<?php echo $picksheet['id']; ?>">Make / View payments</a></td>
 				<?php }else{ ?>
                     <td>Invoice Paid</a></td>  
                 <?php }?>  
@@ -113,7 +112,6 @@
                     ?>
                  </td>
                 <td><?php echo $date; ?></td>
-                <td><a href="javascript:;" onclick="generatePDF(<?php echo $picksheet['id']; ?>)">Invoice_<?php echo $picksheet['id']; ?>.pdf</a></td>
                 <td align="right">£<?php echo number_format($this_price,2,".",","); ?></td>
                 <td align="right">£<?php echo number_format($picksheet['paid'], 2, ".", ","); ?></td>
                 <td align="right">£<?php echo number_format($currentOutstanding, 2, ".", ","); ?></td>
@@ -187,23 +185,3 @@
         height:32px;
     }
 </style>
-
-<script>
-    function generatePDF(id){
-		$.get("<?php echo $domain; ?>ajax/generatePDFInvoice.php?id=" + id, function(data, status){
-			
-			var name = data.replace(/\s+/g, '');
-			
-			downloadURI('<?php echo $domain; ?>PDF/' + name, name);
-			
-		});
-	}
-	
-	function downloadURI(uri, name) 
-	{
-		var link = document.createElement("a");
-		link.download = name;
-		link.href = uri;
-		link.click();
-	}
-</script>
