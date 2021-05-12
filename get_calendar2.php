e0fa033a20c4e625e9b2363570aa899b6e056ed0
Kez <kez@devclever.co.uk>
2021-05-12 09:47:03 +0100

    fix: (TC-276) calendar hard coded year date range

--- get_calendar2.php
+++ get_calendar2.php
@@ -88,9 +88,14 @@
 		</select>
 
 		<select name="year" class="calMonth" id="calYear">
-			<option value="2018" <?php if($year == '2018'){ echo 'selected'; } ?>>2018</option>
-			<option value="2019" <?php if($year == '2019'){ echo 'selected'; } ?>>2019</option>
-			<option value="2020" <?php if($year == '2020'){ echo 'selected'; } ?>>2020</option>
+			<?php
+				$yearLoop = date('Y') - 3;
+				
+				while($yearLoop < (date('Y') + 2)){
+					$yearLoop++;
+					?><option value="<?php echo $yearLoop; ?>" <?php if($year == $yearLoop){ echo 'selected'; } ?>><?php echo $yearLoop; ?></option><?php
+				}
+			?>
 		</select>
 	</div>
 	<div>
