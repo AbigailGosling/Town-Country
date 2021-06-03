3a4fd470d538b547b0f9c30689f5b5cabe54fa48
Kez <kez@devclever.co.uk>
2021-06-03 14:59:43 +0100

    fix: (TC-245) load more functionality for purchase list

--- purchaseList.php
+++ purchaseList.php
@@ -28,7 +28,9 @@
 	<div id="intakelist">
 		<h1 class="int">Purchase LIST</h1>
 		<input type="text" id="instantSearch" placeholder="Search.." style="width:260px;height:28px;padding-left:10px;">
-		
+		<input type="hidden" id="toSkipCount" value="0">
+		<input type="hidden" id="totalRowsCount" value="0">
+
 		<a href="purchaseList.php" class="resetBtn">Clear</a>
 		<div class="datesearchcontainer">
 			<label>MONTH</label>
@@ -49,137 +51,18 @@
 			</select>
 		</div>
 		<table width="100%" border="0" cellpadding="0" cellspacing="0" id="intakeAjax">
-			<?php
-				$x = "SELECT * FROM `purchase_form` ORDER BY date_due DESC";
-				$y = mysqli_query($conn, $x) or die(mysqli_error($conn));
-
-				$page_limit = 50;
-				$num_of_pages = 1;
-				$entry_count = 0;
-				while($row = mysqli_fetch_array($y)){
-					$entry_count++;
-
-					if($entry_count == $page_limit){
-						$entry_count = 0;
-						$num_of_pages++;
-					}
-
-				$id = $row['id'];
-				// $ix = "SELECT id from intake WHERE purchase_id='$id'";
-				// $iy = mysqli_query($conn, $ix);
-				// $irow = mysqli_fetch_array($iy);
-				// $iID = $irow['id'];
-				
-				$x1 = "SELECT * FROM `intake` WHERE purchase_id='$id'";
-				$y1 = mysqli_query($conn, $x1);
-				$intake = mysqli_fetch_array($y1);
-				$intakeCount = mysqli_num_rows($y1);
-				
-				// if($count == 0){
-						
-					$date_purchased = date('d/m/Y', strtotime($row['date_due']));
-					?>
-					<tr class="pages page<?php echo $num_of_pages; ?>"><td align="center" class="pos">
-						<a href="createPurchase.php?id=<?php echo $row['id']; ?>" class="intake">
-							<table width="100%" border="0">
-								<tr>
-									<td width="35%" align="left">ID: P-00<?php echo $row['id']; ?> </td>
-									<td width="60%" align="left" style="font-size: 16px;">
-										<?php if($row['direct_drop'] == 1){ echo '<span style="font-size:12px;">[direct drop]</span>'; } ?>
-										<?php echo supplierName($row['supplier_id']); ?>
-										<?php if($row['booking_ref_number'] == ''){ ?><span style="color:red;padding-left:5px;font-size:26px;font-weight:700">!</span><?php } ?>
-										
-										<?php
-											$thisid = $row['id'];
-											
-											$x2 = "SELECT * FROM `intake` WHERE purchase_id='$thisid'";
-											$y2 = mysqli_query($conn, $x2);
-											$count22 = mysqli_num_rows($y2);
-											
-											if($intakeCount != 0){
-											?> <div class="printedLabel">Intake Created</div> <?php
-											}else{
-											?>  <?php
-											}
-										?>
-									</td>
-									<td width="35%" align="right"><?php echo $date_purchased; ?></td>
-									<td width="10%" align="right">
-										<i class="peek-products fa fa-product-hunt" aria-hidden="true"></i>
-										<div class="tooltip-content">
-											<?php
-												$species = explode('|', $row['species']);
-												$cuts = explode('|', $row['cut']);
-												$units = explode('|', $row['units']);
-												$prices = explode('|', $row['price']);
-												
-												$size = sizeof($species);
-											?>
-											<table>
-												<tr>
-													<th>Species</th>
-													<th>Cuts</th>
-													<th>Units</th>
-													<th>Prices</th>
-												</tr>
-												<?php for($i=0; $i < $size; $i++){ ?>
-													<tr>
-														<td><?php echo $species[$i]; ?></td>
-														<td><?php echo $cuts[$i]; ?></td>
-														<td><?php echo $units[$i]; ?></td>
-														<td><?php echo $prices[$i]; ?></td>
-													</tr>
-													<?php } ?>
-											</table>
-										</div>
-									</td>
-								</tr>
-							</table>
-						</a>
-						
-						 
-						<a href="javascript:;" onclick="deleteRow('<?php echo $row['id'];?>')" id="delete_intake"><i class="fa fa-times" aria-hidden="true"></i></a>
-
-					</td></tr>
-					<?php
-				// }
-				}
-			?>
-			<tr>
-				<td>
-					<div class="pages_container">
-						<div class="flex" style="align-items:center;justify-content:flex-end;">
-							<p style="color:#fff;padding-right:10px;font-weight:bold">Jump to page</p>
-							<?php $num_of_pages_temp = $num_of_pages+1; ?>
-							<select style="width:60px;height:30px;" onchange="changePage(this)">
-								<?php for($i=1;$i<($num_of_pages_temp); $i++){ ?>
-									<option value="<?php echo $i; ?>"><?php echo $i; ?></option>
-								<?php } ?>
-							</select>
-						</div>
-					</div>
-				</td>
-			</tr>
+			 
 		</table>
-		</table>
+		<div class="loadMoreBtn" onclick="loadRows()">Load More</div>
 	</div>
 </main>
 <div id="btm"></div>
 	<script type="text/javascript">
-
-		function changePage(ele){
-			var page = $(ele).val();
-			$('.pages').hide();
-			$('.page' + page).fadeIn();
-		}
-
-		function loadPage(page){
-			$('.pages').hide();
-			$('.page' + page).fadeIn();
-		}
-
 		$(document).ready(function(){
-			loadPage(1);
+			
+			// load initial 80 rows
+			loadRows();
+
 			$('#instantSearch').keyup(function(){
 
 				var val = $('#instantSearch').val();
@@ -189,7 +72,6 @@
 				xhttp.onreadystatechange = function() {
 				if (this.readyState == 4 && this.status == 200) {
 					$('#intakeAjax').html(this.responseText);
-					loadPage(1);
 				}
 				};
 
@@ -230,6 +112,34 @@
 			
 		});
 		
+		function loadRows(){
+			
+			var toSkip = $('#toSkipCount').val();
+			
+			var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				$('#intakeAjax').append(this.responseText);
+				
+
+				setTimeout(() => {
+					var toSkip = parseInt($('#toSkipCount').val());
+					var totalRowsCount = parseInt($('#totalRowsCount').val());
+
+					if(toSkip >= totalRowsCount){
+						$('.loadMoreBtn').hide();
+					}else{
+						$('.loadMoreBtn').show();
+					}
+				}, 1000);
+			}
+			};
+
+			xhttp.open("POST", "/ajax/page-list/purchaseList.php", true);
+			xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhttp.send("toSkip=" + toSkip);
+		}
+
 		function loadSearchDate(month, year){
 			
 			$('#instantSearch').val('');
