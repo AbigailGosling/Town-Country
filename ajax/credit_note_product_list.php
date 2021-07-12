e8b105f0e0b2e3bbfe80d3cfcd6721ff89159f6a
Kez <kez@devclever.co.uk>
2021-07-12 13:06:34 +0100

    fix: (TC-322) original invoice data to show on create credit note

--- ajax/credit_note_product_list.php
+++ ajax/credit_note_product_list.php
@@ -11,7 +11,147 @@
     if($countReturnedIntakes == 0){ # no returned intakes for this invoice
         echo '<b style="color:red;font-size:18px;">No returned intakes for Invoice #' . $invoiceID . ' was found</b>';
     }else{
-        ?>           
+        ?>
+        
+        
+<div style="background:#f2f2f2;padding:10px;">
+<h2 style="font-size:22px;padding-bottom:10px;">Original Invoice</h2>
+<table width="100%" border="0">
+<tr style="border-bottom:1px solid #f1f1f1;">
+    <th align="left">Intake ID</th>
+    <th align="left">Pallet ID</th>
+    <th align="left">Product</th>
+    <th align="left">Quantity</th>
+    <th align="left">Unit</th>
+    <th align="left">Weight</th>
+    <th align="left">Price</th>
+    <th align="left"></th>
+</tr>
+<?php
+
+    $outpalletResult = mysqli_query($conn, "SELECT * FROM `palletsOut` WHERE pickersheet_id='$invoiceID'");
+    $outpalletCount = mysqli_num_rows($outpalletResult);
+
+    $total_weight_count = 0;
+    $total_case_count = 0;
+    $rowCount = 0;
+
+    while($outpallet = mysqli_fetch_array($outpalletResult)){
+        $rowCount++;
+        $rowClass = "productRow" . $rowCount;
+
+        $weightids = explode(',', $outpallet['weight_ids']);
+
+        $productIDArray = array();
+
+        foreach($weightids as $weightid){
+            $x = "SELECT * FROM `weights` WHERE id='$weightid'";
+            $y = mysqli_query($conn, $x);
+            $weight = mysqli_fetch_array($y);
+
+            if(!in_array($weight['product_id'], $productIDArray)){
+                array_push($productIDArray, $weight['product_id']);
+            }
+
+            $queryBits .= ' id = ' . $weightid . ' || ';
+        }
+        $kg = 0;
+        foreach($productIDArray as $productID){
+
+            $x1 = "SELECT * FROM `product` WHERE id='$productID'";
+            $y1 = mysqli_query($conn, $x1);
+            $product = mysqli_fetch_array($y1);
+
+
+            if($product['unit'] == 'PPC'){
+                $ext = ' Cases';
+            }else{
+                $ext = ' kg';
+            }
+
+            $x2 = "SELECT * FROM `weights` WHERE ";
+
+            foreach($weightids as $weightid){
+                $x2 .= "product_id='$productID' && id='$weightid' || ";
+            }
+
+            $x2 = rtrim($x2," || ");
+            $y2 = mysqli_query($conn, $x2);
+            $count = mysqli_num_rows($y2);
+            
+             
+            
+            while($weightRow = mysqli_fetch_array($y2)){               
+                if($weightRow['weight_tear'] == $weightRow['weight_gross']){
+                    $tw = $weightRow['weight_gross'];
+                }else{
+                    $tw = $weightRow['weight_gross'] - $weightRow['weight_tear'];
+                }
+                
+                $kg = $kg + $tw;
+                
+                $kg = number_format($kg, 2, '.', '');
+            }
+
+        ?>
+        <tr class="<?php echo $rowClass; ?>" style="height:50px;border-bottom:1px solid #f1f1f1;">
+            <td align="left"><span class=""><?php echo intakeIDfromPalletID($product['pallet_id']); ?></span></td>
+            <td align="left"><span class=""><?php echo $product['pallet_id']; ?></span></td>
+            <td align="left">                    
+                <span class=""><?php echo getNationality($product['nationality_id']); ?></span>
+                <span class=""><?php echo getTemp($product['cooling_id']); ?></span>
+                <b class=""><?php echo getSpeciesFromCutID($product['cut_id']); ?></b>
+                <b class=""><?php echo getCut($product['cut_id']); ?></b>
+                <b class=""><?php echo getBrand($product['brand_id']); ?></b>
+            </td>
+
+            <?php
+                $productID = $product['id'];
+                $howManyX = "SELECT * FROM `pickerItems` WHERE pickersheet_id='$invoiceID' AND product_id='$productID'";
+                $howManyY = mysqli_query($conn, $howManyX);
+                $pickerItem = mysqli_fetch_array($howManyY);
+                $howMany = mysqli_num_rows($howManyY);
+            ?>
+            <td align="left"><b class="">
+                <b><?php echo $count; ?></b>
+            </td>
+            <td align="left">
+                <b class="s">
+                <?php
+
+                    if($product['unit'] == 'C'){
+                        $unit = 'Cases';
+                    }else if($product['unit'] == 'PPC'){
+                        $unit = 'Per Case';
+                    }else if($product['unit'] == 'P'){
+                        $unit = 'Pallet';
+                    }else if($product['unit'] == 'KG'){
+                        $unit = 'Kilo';
+                    }else{
+                        $unit = 'Cases';
+                    }
+
+                    echo $unit;
+                ?>
+                </b>
+            </td>
+            <td>
+                <?php echo $kg; ?> kg
+            </td>
+             
+            <td align="left" class="">£<input type="number" disabled style="outline:none;border:0;border-bottom:1px dashed black;width:100px;margin-left:10px;" value="<?php echo number_format((float)$pickerItem['price'], 2, '.', ''); ?>"></td>
+            <td>
+            </td>
+        </tr>
+        <?php
+            }
+        }
+    ?>
+</table> 
+</div>
+<Br/>
+<h2 style="font-size:22px;padding-bottom:10px;">Returned Products</h2>
+
         <table width="75%" border="0">
         <tr style="border-bottom:1px solid #f1f1f1;">
             <th align="left">Intake ID</th>
@@ -85,3 +225,5 @@
     }
 ?>
 </table>
+
+<br/>
