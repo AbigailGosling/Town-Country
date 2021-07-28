447f560a1ac0d3e9b4b619ddac71edf453966db3
Kez <kez@devclever.co.uk>
2021-07-28 12:57:35 +0100

    fix: (TC-359) removed freehand salesman field

--- scripts/addCustomer.php
+++ scripts/addCustomer.php
@@ -63,11 +63,7 @@
 	
 	
 	$customer_email = mysqli_real_escape_string($conn, $_POST['customer_email']);
-	
 
-	
-	$salesman = mysqli_real_escape_string($conn, $_POST['salesman']);
-	
 	
 	$companyregno = mysqli_real_escape_string($conn, $_POST['companyregno']);
 	
@@ -121,9 +117,9 @@
 	$default_salesman_id = mysqli_real_escape_string($conn, $_POST['default_salesman_id']);
 	
  	
-	$x = "INSERT INTO `customers` (`businessname`, `tradingas`, `address1_1`, `address1_2`, `address1_3`, `address1_4`, `postcode_1`, `address2_1`, `address2_2`, `address2_3`, `address2_4`, `postcode_2`, `address3_1`, `address3_2`, `address3_3`, `address3_4`, `postcode_3`, `nameofbuyer`, `contactnumber`, `customer_email`, `salesman`, `companyregno`, `accounts_address_1`, `accounts_address_2`, `accounts_address_3`, `accounts_address_4`, `accounts_contact`, `tel_number`, `internal_email`, `credit_terms`, `pricedefault`, `credit_rating`, `flaguplimit`, `current_outstanding`,`address1_number`,`address2_number`,`address3_number`,`accounts_email`,`accounts_comments`,`default_salesman_id`) 
+	$x = "INSERT INTO `customers` (`businessname`, `tradingas`, `address1_1`, `address1_2`, `address1_3`, `address1_4`, `postcode_1`, `address2_1`, `address2_2`, `address2_3`, `address2_4`, `postcode_2`, `address3_1`, `address3_2`, `address3_3`, `address3_4`, `postcode_3`, `nameofbuyer`, `contactnumber`, `customer_email`, `companyregno`, `accounts_address_1`, `accounts_address_2`, `accounts_address_3`, `accounts_address_4`, `accounts_contact`, `tel_number`, `internal_email`, `credit_terms`, `pricedefault`, `credit_rating`, `flaguplimit`, `current_outstanding`,`address1_number`,`address2_number`,`address3_number`,`accounts_email`,`accounts_comments`,`default_salesman_id`) 
 	VALUES
-	('$businessname','$tradingas','$address1_1','$address1_2','$address1_3','$address1_4','$postcode_1','$address2_1','$address2_2','$address2_3','$address2_4','$postcode_2','$address3_1','$address3_2','$address3_3','$address3_4','$postcode_3','$nameofbuyer','$contactnumber','$customer_email','$salesman','$companyregno','$accounts_address_1','$accounts_address_2','$accounts_address_3','$accounts_address_4','$accounts_contact','$tel_number','$internal_email','$credit_terms','$pricedefault','$credit_rating','$flaguplimit','$current_outstanding','$address1_number','$address2_number','$address3_number','$accounts_email','$accounts_comments','$default_salesman_id');";
+	('$businessname','$tradingas','$address1_1','$address1_2','$address1_3','$address1_4','$postcode_1','$address2_1','$address2_2','$address2_3','$address2_4','$postcode_2','$address3_1','$address3_2','$address3_3','$address3_4','$postcode_3','$nameofbuyer','$contactnumber','$customer_email','$companyregno','$accounts_address_1','$accounts_address_2','$accounts_address_3','$accounts_address_4','$accounts_contact','$tel_number','$internal_email','$credit_terms','$pricedefault','$credit_rating','$flaguplimit','$current_outstanding','$address1_number','$address2_number','$address3_number','$accounts_email','$accounts_comments','$default_salesman_id');";
 	
 	$y = mysqli_query($conn, $x) or die(mysqli_error($conn));
 	
--- scripts/updateCustomer.php
+++ scripts/updateCustomer.php
@@ -72,10 +72,6 @@
 	
 	$customer_email = mysqli_real_escape_string($conn, $_POST['customer_email']);
 	
-
-	$salesman = mysqli_real_escape_string($conn, $_POST['salesman']);
-	
-	
 	$companyregno = mysqli_real_escape_string($conn, $_POST['companyregno']);
 	
 
@@ -128,7 +124,7 @@
 	$x = "UPDATE `customers` SET businessname='$businessname', tradingas='$tradingas', address1_1='$address1_1', address1_2='$address1_2', address1_3='$address1_3'
 	, address1_4='$address1_4', postcode_1='$postcode_1', address2_1='$address2_1', address2_2='$address2_2', address2_3='$address2_3', address2_4='$address2_4'
 	, postcode_2='$postcode_2', address3_1='$address3_1', address3_2='$address3_2', address3_3='$address3_3', address3_4='$address3_4', postcode_3='$postcode_3'
-	, nameofbuyer='$nameofbuyer', contactnumber='$contactnumber', customer_email='$customer_email', salesman='$salesman', companyregno='$companyregno', accounts_address_1='$accounts_address_1'
+	, nameofbuyer='$nameofbuyer', contactnumber='$contactnumber', customer_email='$customer_email', companyregno='$companyregno', accounts_address_1='$accounts_address_1'
 	, accounts_address_2='$accounts_address_2', accounts_address_3='$accounts_address_3', accounts_address_4='$accounts_address_4', accounts_contact='$accounts_contact'
 	, tel_number='$tel_number', internal_email='$internal_email', credit_terms='$credit_terms', pricedefault='$pricedefault', credit_rating='$credit_rating', flaguplimit='$flaguplimit'
 	, current_outstanding='$current_outstanding',address1_number='$address1_number',address2_number='$address2_number',address3_number='$address3_number'
