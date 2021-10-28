1e5f61cd4942ec964c9ae7f2d376797c969874f1
LAPTOP-NQ1U7MT3\shama <shamain.kolourbox@gmail.com>
2021-10-28 10:55:47 +0530

    (fix) - revert balance calculation

--- single_invoice_payments.php
+++ single_invoice_payments.php
@@ -84,7 +84,7 @@ if (!empty($paymentID)) {
                     <?php
                         if($invoicePayment['payment_method'] == 'CREDIT_NOTE'){
                             $credit_note_total = creditNoteTotal($invoicePayment['id']);
-                            //$runningBalance -= $credit_note_total;
+                            $runningBalance -= $credit_note_total;
                             echo '<span style="color:green;font-weight:bold">+</span> £' . number_format($credit_note_total, 2, ".", ",");
                         }else{
                             $runningBalance -= $invoicePayment['amount'];
