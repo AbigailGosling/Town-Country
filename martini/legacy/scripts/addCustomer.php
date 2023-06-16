3fd02a1494cf9e0824fd6e6726c3e77de2bcd1ef
Abigail Gosling <abigail.gosling@tang.solutions>
2023-06-16 16:08:12 +0100

    changes to traffic lights and transact id

--- martini/legacy/scripts/addCustomer.php
+++ martini/legacy/scripts/addCustomer.php
@@ -4,99 +4,99 @@
 	$colValue = array();
 
 	$colNames[] = "`businessname`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('businessname'));
+	$colValue[] = request()->input('businessname');
 	
 	$colNames[] = "`tradingas`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('tradingas'));
+	$colValue[] = request()->input('tradingas');
 	
 	$colNames[] = "`nameofbuyer`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('nameofbuyer'));
+	$colValue[] = request()->input('nameofbuyer');
 	
 	$colNames[] = "`contactnumber`";	
-	$colValue[] = $mysqli->real_escape_string( request()->input('contactnumber'));
+	$colValue[] = request()->input('contactnumber');
 	
 	$colNames[] = "`customer_email`";	
-	$colValue[] = str_replace(array("\r", "\n"), '', $mysqli->real_escape_string( request()->input('customer_email')));
+	$colValue[] = str_replace(array("\r", "\n"), '', request()->input('customer_email'));
 
 	$colNames[] = "`companyregno`";	
-	$colValue[] = $mysqli->real_escape_string( request()->input('companyregno'));
+	$colValue[] = request()->input('companyregno');
 	
 	$colNames[] = "`accounts_address_1`";	
-	$colValue[] = $mysqli->real_escape_string( request()->input('accounts_address_1'));
+	$colValue[] = request()->input('accounts_address_1');
 	
 	$colNames[] = "`accounts_address_2`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('accounts_address_2'));
+	$colValue[] = request()->input('accounts_address_2');
 	
 	$colNames[] = "`accounts_address_3`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('accounts_address_3'));
+	$colValue[] = request()->input('accounts_address_3');
 	
 	$colNames[] = "`accounts_address_4`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('accounts_address_4'));
+	$colValue[] = request()->input('accounts_address_4');
 	
 	$colNames[] = "`accounts_contact`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('accounts_contact'));
+	$colValue[] = request()->input('accounts_contact');
 	
 	$colNames[] = "`tel_number`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('tel_number'));
+	$colValue[] = request()->input('tel_number');
 	
 	$colNames[] = "`internal_email`";
-	$colValue[] = str_replace(array("\r", "\n"), '', $mysqli->real_escape_string( request()->input('internal_email')));
+	$colValue[] = str_replace(array("\r", "\n"), '', request()->input('internal_email'));
 	
 	$colNames[] = "`credit_terms`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('credit_terms'));
+	$colValue[] = request()->input('credit_terms');
 	
 	$colNames[] = "`pricedefault`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('pricedefault'));
+	$colValue[] = request()->input('pricedefault');
 	
 	$colNames[] = "`credit_rating`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('credit_rating'));
+	$colValue[] = request()->input('credit_rating');
 	
 	$colNames[] = "`flaguplimit`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('flaguplimit'));
+	$colValue[] = request()->input('flaguplimit');
 		
 	$colNames[] = "`current_outstanding`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('current_outstanding'));
+	$colValue[] = request()->input('current_outstanding');
 
 	$colNames[] = "`accounts_email`";
-	$colValue[] = str_replace(array("\r", "\n"), '', $mysqli->real_escape_string( request()->input('accounts_email')));
+	$colValue[] = str_replace(array("\r", "\n"), '', request()->input('accounts_email'));
 	
 	$colNames[] = "`accounts_comments`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('accounts_comments'));
+	$colValue[] = request()->input('accounts_comments');
 	
 	$colNames[] = "`default_salesman_id`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('default_salesman_id'));
+	$colValue[] = request()->input('default_salesman_id');
 	
 	$colNames[] = "`due_warning`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('due_warning'));
+	$colValue[] = request()->input('due_warning');
 
 	$colNames[] = "`credit_grace`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('credit_grace'));
+	$colValue[] = request()->input('credit_grace');
 
 	$colNames[] = "`markup_type`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('markup_type'));
+	$colValue[] = request()->input('markup_type');
 
 	$colNames[] = "`markup_amount`";
-	$colValue[] = $mysqli->real_escape_string( request()->input('markup_amount'));
+	$colValue[] = request()->input('markup_amount');
 
 	for ($u=1;$u<10;$u++)
 	{
 		$colNames[] = '`address'.$u.'_1`';
-		$colValue[] = "'".$mysqli->real_escape_string( request('address'.$u.'_1'))."'";
+		$colValue[] = "'".request()->input('address'.$u.'_1')."'";
 
 		$colNames[] = '`address'.$u.'_2`';
-		$colValue[] = "'".$mysqli->real_escape_string( request('address'.$u.'_2'))."'";
+		$colValue[] = "'".request()->input('address'.$u.'_2')."'";
 
 		$colNames[] = '`address'.$u.'_3`';
-		$colValue[] = "'".$mysqli->real_escape_string( request('address'.$u.'_3'))."'";
+		$colValue[] = "'".request()->input('address'.$u.'_3')."'";
 
 		$colNames[] = '`address'.$u.'_4`';
-		$colValue[] = "'".$mysqli->real_escape_string( request('address'.$u.'_4'))."'";
+		$colValue[] = "'".request()->input('address'.$u.'_4')."'";
 
 		$colNames[] = '`postcode_'.$u.'`';
-		$colValue[] = "'".$mysqli->real_escape_string( request('postcode_'.$u))."'";
+		$colValue[] = "'".request()->input('postcode_'.$u)."'";
 
 		$colNames[] = '`address'.$u.'_number`';
-		$colValue[] = "'".$mysqli->real_escape_string( request('address'.$u.'_number'))."'";
+		$colValue[] = "'".request()->input('address'.$u.'_number')."'";
 	}
 
 	$x = "INSERT INTO `customers` (".implode(",",$colNames).") 
