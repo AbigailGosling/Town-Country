bf397d668701ad76cf772d477135f6bbf632f9c5
Abigail Gosling <abigail.gosling@tang.solutions>
2024-11-11 10:46:09 +0000

    remove api keys, regenerated on services

--- martini/legacy/scripts/SLabsNotifier.php
+++ martini/legacy/scripts/SLabsNotifier.php
@@ -4,7 +4,7 @@ require_once(__DIR__.'/../scripts/SLabsEmailer.php');
 use InternalScripts\SLabsEmailer;
 
 $data = json_decode(file_get_contents('php://input'), true);
-if ($data['SecretKey'] != SLabsEmailer::NotifcationAPIKey) 
+if ($data['SecretKey'] != env("MAIL_NOTIFICATION_KEY"))
 {
     http_response_code(401);
 }
