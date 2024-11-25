a0fa021cd27f91f6843de9c26825ee03c26e789a
Abigail Gosling <abigail.gosling@tang.solutions>
2024-11-25 10:47:15 +0000

    added del date and day enforcement

--- martini/legacy/viewSalesconfirmation.php
+++ martini/legacy/viewSalesconfirmation.php
@@ -8,26 +8,26 @@ use Illuminate\Support\Facades\Auth;
 	if ($adv == false) include_once('includes/frontHeader.php');
 	else require_once('functions.php');
 
-	
+
 	$picksheet_id = request()->input('id');
 
 	$x = "SELECT * FROM `pickerSheets` WHERE id=?";
 	$y = prepareExecuteQuery($x,'i',[$picksheet_id]);
 	$picksheet = mysqli_fetch_array($y);
-	
-	
-	
+
+
+
 	$customer_id = $picksheet['customer_id'];
 	$x1 = "SELECT * FROM `customers` WHERE id=?";
 	$y1 = prepareExecuteQuery($x1,'i',[$customer_id]);
 	$customer = mysqli_fetch_array($y1);
-	
+
 	$addressNumber = $row['address'.$picksheet['addressid'].'_number'];
-	
+
 	$address = $customer['address'.$picksheet['addressid'].'_1'];
 	if($customer['address'.$picksheet['addressid'].'_2']){ $address .= ',&#13;'; }
 	$address .= $customer['address'.$picksheet['addressid'].'_2'];
-	
+
 	if($customer['address'.$picksheet['addressid'].'_3']){ $address .= ',&#13;'; }
 	$address .= $customer['address'.$picksheet['addressid'].'_3'];
 
@@ -82,9 +82,9 @@ use Illuminate\Support\Facades\Auth;
 			<label>Delivery Date</label><br/>
 			<input class="form-control" type="text" class="inputbox" id="estimated_delivery_date" name="estimated_delivery_date" placeholder="" <?php if (!User::find(Auth::id())->hasPermission("change_sale_details")) {?>disabled<?php }?>>
 		</div>
-	  
+
 	</div>
-	
+
 	<div class="row" id="address"></div>
 
 	<?php if($picksheet['user_from_id'] != ''){ ?>
@@ -103,9 +103,9 @@ use Illuminate\Support\Facades\Auth;
 		</div>
 		<div class="col"></div>
 	</div>
-	
+
 	<?php } ?>
-	
+
 	<div class="row printhide">
 		<div class="col">
 			<label>Picksheet Notes</label><br/>
@@ -113,7 +113,7 @@ use Illuminate\Support\Facades\Auth;
 		</div>
 		<div class="col"></div>
 	</div>
-	
+
 	<div class="row">
 		<div class="col">
 			<label>	Order Reference Number</label><br/>
@@ -125,10 +125,11 @@ use Illuminate\Support\Facades\Auth;
 	<div class="row printhide">
 		<div class="col">
 			<?php if ($usermodel->hasPermission("change_sale_details")||$usermodel->hasPermission("change_sale_reference")) {?>
-			<input type="button" onclick="mainForm()" value="Update">
+			<input type="button" id="sendfake" name="sendfake" onclick="mainForm()" value="Update">
 			<?php }?>
 		</div>
 	</div>
+    <div class="row custom-warning-box" id="warning" style="width: 100%; padding-top:0px; padding-bottom:0px;  padding: left right 15px; display:none"></div>
 </div>
 </form>
 
@@ -136,7 +137,7 @@ use Illuminate\Support\Facades\Auth;
 	<table width="100%" class="basketTable">
 		<tr align="left" style="background:#e6931894;height:30px;color:#FFF;">
 			<th>Intake ID</th>
-			<th>Plt ID</th>	
+			<th>Plt ID</th>
 			<th width="45%">Product</th>
 			<th align="right">Dated</th>
 			<th>Nationality</th>
@@ -146,8 +147,8 @@ use Illuminate\Support\Facades\Auth;
  			<th>Price</th>
 		</tr>
 		<?php
-			
-		
+
+
 			$x = "SELECT * FROM `pickerItems` WHERE pickersheet_id=?";
 			$y = prepareExecuteQuery($x,'i',[$picksheet_id]);
 			$vars = array();
@@ -157,29 +158,31 @@ use Illuminate\Support\Facades\Auth;
 			$query = "SELECT * FROM `product` WHERE id IN (".implode(",",array_fill(0,count($vars),"?")).")";
 
 		?>
-		
-		
+
+
 		<?php
  			$yproduct = prepareExecuteQuery($query,str_repeat('i',count($vars)),$vars);
-			
+
 			while($product = mysqli_fetch_array($yproduct)){
 			?>
 			<tr class="productsRow">
 					<?php
-						
+
 						$thispalletid = $product['pallet_id'];
-						
+
 						$palletx = "SELECT * FROM `pallet` WHERE id =?";
 						$pallety = prepareExecuteQuery($palletx,'i',[$thispalletid]);
 						$pallet = mysqli_fetch_array($pallety);
 						$smallestDate = ($product['range_extension']!= null && $product['range_extension']!= '')?"EXTENSION":$product['range_from'];
 						$largestDate = ($product['range_extension']!= null && $product['range_extension']!= '')?$product['range_extension']:$product['range_to'];
-						
+                        $temp_id = $product['cooling_id'];
+
 					?>
 					<td align="left"><span class="intakeid"><?php echo $pallet['intake_id']; ?></span></td>
 					<td align="left"><span class="palletid"><?php echo $product['pallet_id'];?></span></td>
 					<td align="left"><b class="species"><?php echo getSpeciesFromCutID($product['cut_id']); echo getCut($product['cut_id']);  ?></b></td>
 					<td align="right"><b class="species"><?php echo $smallestDate.'-'.$largestDate; ?></b></td>
+                    <td style="display: none;" id="ubDate"><?php echo $smallestDate; ?></td><td id=temp_id style="display:none;"><?php echo $temp_id; ?></td>
 					<td align="left"><span class="chilled"><?php echo getNationality($product['nationality_id']); ?></span></td>
 					<td align="left"><b class="brand"><?php echo getBrand($product['brand_id']); ?></b></td>
 					<?php
@@ -196,9 +199,9 @@ use Illuminate\Support\Facades\Auth;
 					</td>
 					<td align="left">
 						<b class="unit">
-						 
+
 						<?php
-							
+
 							if($product['unit'] == 'C'){
 								$unit = 'p/KG';
 							}else if($product['unit'] == 'PPC'){
@@ -210,7 +213,7 @@ use Illuminate\Support\Facades\Auth;
 							}else{
 								$unit = 'p/KG';
 							}
-							
+
 							echo $unit;
 						?>
 						</b>
@@ -222,53 +225,54 @@ use Illuminate\Support\Facades\Auth;
 						?>
 					</td>
  					</tr>
-			
+
 			<?php
 			}
 		?>
-		
+
  	</table>
-	
+
 	<div style="float:right;">
 		<br/><br/>
 		<div class="totalprice" style="display:none;"></div>
 		<br/>
   	</div>
 </div>
- 
+
 
 
 <div class="clearfix"></div>
-<?php 
+<?php
 	if(request()->input('msg') != ''){
 	?>
 	<script type="text/javascript">
 		alert('<?php echo request()->input('msg');?>');
 	</script>
-	<?php	
+	<?php
 	}
 ?>
 <script type="text/javascript">
 	$('#customer').attr('disabled', 'disabled');
 	<?php if (User::find(Auth::id())->hasPermission("change_sale_details")) {?>
 	$( "#estimated_delivery_date" ).datepicker({
+            onSelect: ddChanged,
 			dateFormat: 'dd/mm/yy'
 	});
 	<?php } ?>
 	setCustomerDetails(<?php echo $customer_id; ?>,<?php echo $picksheet['addressid']; ?>, 'true');
-	
+
 	setTimeout(() => {
 		$('#customer').val('<?php echo $customer['businessname']; ?>');
 		$('#contactnumber').val('<?php echo $customer['contactnumber']; ?>');
-		$('#estimated_delivery_date').val('<?php echo $picksheet['estimated_delivery_date']; ?>');	
+		$('#estimated_delivery_date').val('<?php echo $picksheet['estimated_delivery_date']; ?>');
 		renderCompleted = true;
 	}, 500);
-	
+
 	$('#customer').keyup(function(){
 		var val = $('#customer').val();
 		$('#customer_search_results').fadeIn();
 
-	 	
+
 		var xhttp = new XMLHttpRequest();
 		xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
@@ -279,7 +283,7 @@ use Illuminate\Support\Facades\Auth;
 		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 		xhttp.setRequestHeader('X-CSRF-TOKEN', "<?php echo csrf_token();?>");
 		xhttp.send("searchterm=" + val);
-	
+
 	});
 	var renderCompleted = false;
 	function setCustomerDetails(customer_id, addressid, empty='false'){
@@ -288,21 +292,91 @@ use Illuminate\Support\Facades\Auth;
 			$('#address').html(data);
 			$('.rating').fadeIn();
 		});
-		
-	}
 
+	}
+    function parseDMY(value) {
+		var date = value.split("/");
+		var d = parseInt(date[0], 10),
+			m = parseInt(date[1], 10),
+			y = parseInt(date[2], 10);
+		return new Date(y, m - 1, d);
+	}
+    var delCheckingOn		= <?php echo ($customer['delivery_day_checking'] == 1 && $customer['delivery_day_override'] == 0)?"true":"false"; ?>;
+	var delDays				= <?php echo ($customer['delivery_days']>0)?$customer['delivery_days']:0; ?>;
+    function ddChanged(dateText){
+		var dateObj = $('#estimated_delivery_date').datepicker('getDate');
+		if (dateObj != null && delCheckingOn){
+			var daySelected = dateObj.getDay();
+			var weekday = 		["Sunday"	,"Monday"	,"Tuesday"	,"Wednesday","Thursday"	,"Friday"	,"Saturday"	];
+			var weekdayLookup = [1			,64			,32			,16			,8			,4			,2			];
+			var weekdayInt = weekdayLookup[daySelected];
+		}
+        console.log([dateObj,delCheckingOn,weekdayInt,delDays]);
+		if (dateObj != null && delCheckingOn && (weekdayInt & delDays) == 0)
+		{
+			day = weekday[daySelected];
+			$('#sendfake').attr('disabled', true);
+			$('#searcher').attr('disabled', true);
+			$('#warning').css('background', "#ff6666");
+			$('#warning').css('border', "2px solid #ff0000");
+			$('#warning').css('display', "inline-block");
+			$('#warning').html("<td align='center' style='height:100%;padding-top:15px;padding-bottom:15px;'>We do not deliver to this customer on "+day+"s</td>");
+			return;
+		}
+
+        var ubs = $('#ubDate');
+        var temps = $('#temp_id');
+        if (ubs.length == 0 || dateText == null || dateText == "") return;
+        var date = dateObj.getTime();
+        $('#sendfake').prop('disabled',false);
+        var beyondBB = false;
+        for(var x = 0; x < ubs.length; x++){
+            var ub = ubs[x];
+            var temp = temps[x];
+            if (customerID == "420") break;
+            if ((ub.innerHTML=="" || temp.innerHTML.trim() != 1))
+            {
+                continue;
+            }
+            var ubd = parseDMY(ub.innerHTML).getTime();
+            if (ubd < date)
+            {
+                $(ub).css('background', "#ff6666");
+                beyondBB = true;
+            }
+            else
+            {
+                $(ub).css('background', "#ffffff");
+            }
+        }
+        if (beyondBB)
+        {
+            $('#sendfake').prop('disabled',true);
+            $('#warning').css('background', "#ff6666");
+            $('#warning').css('border', "2px solid #ff0000");
+            $('#warning').css('display', "inline-block");
+            $('#warning').html("<td align='center' style='height:100%;padding-top:15px;padding-bottom:15px;'>An item in this sale will expire before delivery</td>");
+        }
+        else
+        {
+            $('#warning').css('display', "none");
+            $('#sendfake').attr('disabled', false);
+            $('#searcher').attr('disabled', false);
+        }
+
+	}
 	function addToList(id){
-		
+
 		$.get( "scripts/getBasketItem.php?id="+id, function( data ) {
 			$('.basketTable').append(data);
 		});
-		
+
 	}
-	
+
 	function removeFromList(id, pallet_id, product_id){
 		$('.basketRow-' + id).remove();
 		var COOKIE_NAME = "quantity-"+product_id+"-"+pallet_id;
-		
+
 		document.cookie = COOKIE_NAME + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 	}
 
@@ -321,7 +395,7 @@ use Illuminate\Support\Facades\Auth;
 <style type="text/css">
 	.rightPanel{
 		width:calc(100% - 103px);
-	
+
 		float:left;
 		padding:50px;
 		position:relative;
@@ -335,42 +409,42 @@ use Illuminate\Support\Facades\Auth;
 		border:1px solid #f4f4f4;
 		position:relative;
 	}
-	
+
 	.leftPanel{
 		background:#f2f2f2;
 	}
-	
+
 	.clearfix{
 		clear:both;
 	}
-	
+
 	.inputbox-button{
 		width:323px;
 		height:34px;
 		margin-bottom:10px;
 	}
-	
+
 	.inputbox{
 		width:300px;
 		height:34px;
 		padding-left:18px;
- 
+
 	}
-	
+
 	.createCustomerContainer{
 		font-weight:700;
 		position:absolute;
 		top:50px;
 		right:30px;
 	}
-	
+
 	.weightTotal{
 		font-weight:700;
 		position:absolute;
 		top:50px;
 		right:30px;
 	}
-	
+
 	.resultsContainer{
 		width: calc(100% - 40px);
 		min-height: 400px;
@@ -409,7 +483,7 @@ use Illuminate\Support\Facades\Auth;
 		alert("Sent");
 	}
 	function getRenderResp(data, status){
-        
+
 	}
 	function renderComplete(){
 		return renderCompleted;
