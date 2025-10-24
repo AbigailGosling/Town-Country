e0327f5e04f0551a81611253708941065751e774
Abigail Gosling <abigail.gosling@tang.solutions>
2025-10-24 15:57:17 +0100

    fixes on live

--- martini/legacy/scripts/addCustomer.php
+++ martini/legacy/scripts/addCustomer.php
@@ -66,8 +66,8 @@
 	$colNames[] = "`default_salesman_id`";
 	$colValue[] = request()->input('default_salesman_id');
 
-    $colNames[] = "`check_saledate`";
-	$colValue[] = request()->input('check_saledate',true);
+    //$colNames[] = "`check_saledate`";
+	//$colValue[] = request()->input('check_saledate',true);
 
 	$colNames[] = "`due_warning`";
 	$colValue[] = (request()->input('due_warning')!=null && request()->input('due_warning') != "")?request()->input('due_warning'):"0";
