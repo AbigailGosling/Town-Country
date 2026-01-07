5689319d986714cd36393d02e59210f26eda123e
Abigail Gosling <abigail.gosling@tang.solutions>
2026-01-07 13:46:30 +0000

    add actual cost perm to reservation

--- martini/legacy/scripts/searchContainer.php
+++ martini/legacy/scripts/searchContainer.php
@@ -19,8 +19,8 @@ use Illuminate\Support\Facades\Auth;
 	        <th>Brand</th>
 	        <th class="searchRContent__date-range">ETA</th>
 	        <th>Expected KG</th>
-	        <th>Cost</th>
             <th>RRP</th>
+            <?php if (User::find(Auth::id())->hasPermission("viewcosts")) { ?><th style="color: #cacaca;font-weight: normal;font-size:12px;">Actual Cost</th><?php } ?>
 	        <th class="searchRContent__plus"></th>
         </tr>
     </thead>
@@ -164,8 +164,8 @@ use Illuminate\Support\Facades\Auth;
                             }
                             $prodMod = Product::find($containerProduct->product_id);
                             ?></td>
-                            <td><?php echo "£" . number_format($prodMod->cost, 2, '.', ''); ?></td>
                             <td><?php echo '£' . number_format($containerProduct->rrp, 2, '.', ''); ?></td>
+                            <?php if (User::find(Auth::id())->hasPermission("viewcosts")) { ?><td class="bold" style="font-weight:normal;font-size:10px;"><?php if($prodMod->cost){ echo '£' . number_format((float)$prodMod->cost, 2, '.', ''); } ?></td><?php } ?>
                             <td>
                                 <a href="javascript:;" class="plusButton" onclick="addToSheet('<?php echo $productsRow2['productid']; ?>','<?php echo $productsRow2['pallet_id']; ?>','<?php echo $productsRow2['cut_id']; ?>','<?php echo $class; ?>','<?php echo $largestDate; ?>');"><i class="fa fa-plus" style="font-size:24px;color:#000;"></i></a>
                             </td>
