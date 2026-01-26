22bebf5d9a792f809a23ac5f60487217ead408b3
Abigail Gosling <abigail.gosling@tang.solutions>
2026-01-26 10:02:25 +0000

    Refactor input variable names for consistency in updateSalesReservation.php

--- martini/legacy/scripts/updateSalesReservation.php
+++ martini/legacy/scripts/updateSalesReservation.php
@@ -4,19 +4,17 @@ use Carbon\Carbon;
 
 	require(__DIR__.'/../functions.php');
 
-    $addressid = request()->input('addressid');
-    $customerid = request()->input('customerid');
+    $addressid = request()->input('address_id');
+    $customerid = request()->input('customer_id');
     $picksheetid = request()->input('picksheetid');
     $picksheet_note = request()->input('picksheet_note');
-    $user_from_id = request()->input('user_from_id');
-
+    $user_from_id = request()->input('user_id');
     $eta = Carbon::createFromFormat('d/m/Y', request()->input('eta'));
 
     $orderReferenceNumber = request()->input('orderReferenceNumber');
 
-
     $y = prepareExecuteQuery("UPDATE `reservation` SET `user_id`=?, `order_reference_number`=?, `address_id`=?,`picksheet_note`=?,`eta`=? WHERE id=? LIMIT 1",
-'issss',[$user_from_id,$orderReferenceNumber,$addressid,$picksheet_note,$eta->format("Y-m-d"),$picksheetid]);
+'isssss',[$user_from_id,$orderReferenceNumber,$addressid,$picksheet_note,$eta->format("Y-m-d"),$picksheetid]);
 
     loggedDataChange("reservation_note",$picksheetid,$picksheet_note);
     loggedDataChange("reservation_orderReferenceNumber",$picksheetid,$orderReferenceNumber);
