908177e483ff3e2480b9dc52da224c6e78b090ce
Abigail Gosling <abigail.gosling@tang.solutions>
2026-05-21 10:46:17 +0100

    Refactor saveLocation.php to improve code readability by removing unnecessary whitespace and organizing imports

--- martini/legacy/ajax/saveLocation.php
+++ martini/legacy/ajax/saveLocation.php
@@ -1,29 +1,24 @@
 <?php
 
-use App\Models\InternalPalletMovement;
-use App\Models\Location;
-use App\Models\Pallet;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
+    use App\Models\InternalPalletMovement;
+    use App\Models\Location;
+    use App\Models\Pallet;
+    use Illuminate\Support\Facades\Auth;
 
 	require(__DIR__.'/../functions.php');
 	$pallet = request()->input('pallet');
 	$location = trim(request()->input('location'));
-
     $palletRecord = Pallet::find($pallet);
     if (!$palletRecord) {
         exit;
     }
     $fromLocation = Location::find($palletRecord->storage_location);
     $toLocation = Location::find($location);
-
     if (!$toLocation || $toLocation->id == $palletRecord->storage_location) {
         exit;
     }
-
 	$palletRecord->storage_location = $location;
     $palletRecord->save();
-
 	if ($palletRecord && $palletRecord->storage_location != null && $palletRecord->storage_location !="") {
 	    $internalMovement = new InternalPalletMovement();
 	    $internalMovement->pallet_id = $palletRecord->id;
@@ -33,6 +28,4 @@ use Illuminate\Support\Facades\Log;
 	    $internalMovement->movement_initiated_by = Auth::id();
 	    $internalMovement->save();
 	}
-
-
 ?>
