c8eff768ec26166e886cb6fb8138ee97aae8f7b7
Abigail Gosling <abigail.gosling@tang.solutions>
2026-07-13 12:31:04 +0100

    Remove redundant alteration of outgoing pallet type ID in migration

--- martini/database/migrations/2026_07_06_133047_rename_outgoing_pallet_tables.php
+++ martini/database/migrations/2026_07_06_133047_rename_outgoing_pallet_tables.php
@@ -20,7 +20,6 @@ return new class extends Migration
         DB::connection('tandc_live')->unprepared('RENAME TABLE `vehicle_outgoing_pallet_allocations` TO `vehicle_transport_pallet_allocations`');
 
         DB::connection('tandc_live')->unprepared('ALTER TABLE `transport_pallets` CHANGE `outgoing_pallet_type_id` `transport_pallet_type_id` BIGINT(20) UNSIGNED NOT NULL;');
-        DB::connection('tandc_live')->unprepared('ALTER TABLE `transport_pallet_types` CHANGE `outgoing_pallet_type_id` `transport_pallet_type_id` BIGINT(20) UNSIGNED NOT NULL;');
         DB::connection('tandc_live')->unprepared('ALTER TABLE `transport_pallet_pick_weights` CHANGE `outgoing_pallet_id` `transport_pallet_id` BIGINT(20) UNSIGNED NOT NULL;');
         DB::connection('tandc_live')->unprepared('ALTER TABLE `vehicle_transport_pallet_allocations` CHANGE `outgoing_pallet_id` `transport_pallet_id` BIGINT(20) UNSIGNED NOT NULL;');
 
