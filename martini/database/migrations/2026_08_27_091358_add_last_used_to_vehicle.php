ef6a5ab50d3e2efc67488a39c1f52e3453946c5a
Abigail Gosling <abigail.gosling@tang.solutions>
2026-09-01 11:37:41 +0100

    feat: Add last_used timestamp column to vehicle table migration

---
+++ martini/database/migrations/2026_08_27_091358_add_last_used_to_vehicle.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('tandc_live')->table('vehicle', function (Blueprint $table) {
+            $table->timestamp('last_used')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('tandc_live')->table('vehicle', function (Blueprint $table) {
+            $table->dropColumn('last_used');
+        });
+    }
+};
